edb0b633505136a21e0f3d1cf42cdd1cee1d600e
陈国雄 <bear_swallow@163.com>
2017-12-01 00:03:15 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# docker-superset
+Dockerfile and configuration files for superset web server, using following settings.
+- mysql as metadata storage
+- redis as result cache
+- redis as asyc query message broker
+- redis as asyc query result backend
+
+# docker specification
+The docker image should be used together with [docker-superset-init](https://hub.docker.com/r/malebear311/docker-superset-init/) and [docker-superset-worker](). Use *docker-superset-init* as setup that is only execute once.
+
+```
+docker run -d --name=superset -p 8088:8088 malebear311/docker-superset-init
+```
+
+You can specify following ENVs when execute docker run command. So the container will create with your config.
+- SUPERSET_METADATA_CONNECTION: mysql url of superset metadata storage, must like *mysql://root:gt86589089@galera-lb.galera:3306/superset*.
+- APPLICATION_PREFIX: key prefix of data cache and async query result.
+- CACHE_REDIS_URL: redis url of data cache, must like *redis://redis-master.Redis-cluster:6379/1*.
+- BROKER_URL: redis url of async message broker, must like *redis://redis-master.Redis-cluster:6379/1*.
+- CELERY_RESULT_BACKEND: redis url of async query result storage, must like *redis://redis-master.Redis-cluster:6379/1*.
---
+++ superset_config.py
@@ -0,0 +1,71 @@
+import os
+
+#---------------------------------------------------------
+# Superset specific config
+#---------------------------------------------------------
+#ROW_LIMIT = 5000
+#SUPERSET_WORKERS = 4
+
+#SUPERSET_WEBSERVER_PORT = 8088
+#---------------------------------------------------------
+
+#---------------------------------------------------------
+# Flask App Builder configuration
+#---------------------------------------------------------
+# Your App secret key
+#SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'
+
+# The SQLAlchemy connection string to your database backend
+# This connection defines the path to the database that stores your
+# superset metadata (slices, connections, tables, dashboards, ...).
+# Note that the connection information to connect to the datasources
+# you want to explore are managed directly in the web UI
+SQLALCHEMY_DATABASE_URI = os.getenv('SUPERSET_METADATA_CONNECTION')
+
+# Flask-WTF flag for CSRF
+#WTF_CSRF_ENABLED = True
+# Add endpoints that need to be exempt from CSRF protection
+#WTF_CSRF_EXEMPT_LIST = []
+
+# Set this API key to enable Mapbox visualizations
+#MAPBOX_API_KEY = ''
+
+applicationPrefix = os.getenv('APPLICATION_PREFIX')
+
+# Cache config
+CACHE_CONFIG = {
+    'CACHE_TYPE': 'redis', # 使用 Redis
+    'CACHE_KEY_PREFIX': applicationPrefix + '_cache:', # 缓存项前缀
+    'CACHE_REDIS_URL': os.getenv('CACHE_REDIS_URL') # 配置 URL
+}
+
+resultBackend = os.getenv('CELERY_RESULT_BACKEND')
+# Celery work config, supporting async query
+class CeleryConfig(object):
+    BROKER_URL = os.getenv('BROKER_URL')
+    CELERY_IMPORTS = 'superset.sql_lab'
+    CELERY_RESULT_BACKEND = resultBackend
+    CELERY_ANNOTATIONS = {'tasks.add': {'rate_limit': '10/s'}}
+CELERY_CONFIG = CeleryConfig
+
+# config results backend
+import re
+m = re.match(r'redis://([^:/]+):([0-9]+)/([0-9]+)', resultBackend)
+backendHost = m.group(1)
+backendPort = int(m.group(2))
+backDb = int(m.group(3))
+from werkzeug.contrib.cache import RedisCache
+RESULTS_BACKEND = RedisCache(
+    host=backendHost, port=backendPort, db=backDb,
+    key_prefix= applicationPrefix + "_results:")
+
+# log environment variables for check
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("supersetConfig")
+logger.info("metadata storage: " + SQLALCHEMY_DATABASE_URI)
+logger.info("cache reidsUrl: " + CACHE_CONFIG['CACHE_REDIS_URL'])
+logger.info("broker url: " + CeleryConfig.BROKER_URL)
+logger.info("Result backend url:" + CeleryConfig.CELERY_RESULT_BACKEND)
+logger.info("RedisCache: { host: %s, port: %s, db: %s, key_prefix: %s }" % tuple([backendHost, backendPort, backDb, RESULTS_BACKEND.key_prefix]))
