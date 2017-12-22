34bd22ffea2a072a7653db1d6a6bc32f312c3158
陈国雄 <bear_swallow@163.com>
2017-12-22 15:05:02 +0800

    添加匿名访问slice的配置和导出csv文件的编码为utf-8-sig，让其适合excel打开

--- superset_config.py
+++ superset_config.py
@@ -15,6 +15,15 @@ import os
 # Your App secret key
 #SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'
 
+# Allow users of Public role accessing anonymous
+PUBLIC_ROLE_LIKE_GAMMA=True
+
+# CSV Options: key/value pairs that will be passed as argument to DataFrame.to_csv method
+# note: index option should not be overridden
+CSV_EXPORT = {
+    'encoding': 'utf-8-sig',
+}
+
 # The SQLAlchemy connection string to your database backend
 # This connection defines the path to the database that stores your
 # superset metadata (slices, connections, tables, dashboards, ...).
